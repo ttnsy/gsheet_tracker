24593f6fa80118c65ede3678160686137670e991
Tanesya <tanesya.t@gmail.com>
2023-12-13 22:50:39 +0700

    chore: add bukti unit test

--- tests/testthat/test-bukti.R
+++ tests/testthat/test-bukti.R
@@ -1,4 +1,6 @@
 box::use(
+  glue[glue],
+  htmltools[...],
   testthat[...],
   fixtuRes[random_numeric, random_datetime, random_string]
 )
@@ -7,7 +9,7 @@ box::use(
   app/logic/bukti[...]
 )
 
-has_thousand_separator_with_decimals <- function(x) {
+has_thousand_sep_with_two_dec <- function(x) {
   #' Regular expression to match a consistent thousand separator (dot or comma)
   #' and an optional decimal part with exactly two digits
   #' Handles both English (1,000.00) and European (1.000,00) conventions
@@ -16,7 +18,7 @@ has_thousand_separator_with_decimals <- function(x) {
 }
 
 describe("format_bukti_value()", {
-  rand_num <- round(random_numeric(),2)
+  rand_num <- round(random_numeric(), 2)
   rand_dttm <- random_datetime("2023-01-01", "2023-01-12")
 
   it("formats numbers with 'Rp.' prefix", {
@@ -30,14 +32,14 @@ describe("format_bukti_value()", {
 
   it("formats numbers with thousand separators and two decimal rounding", {
     expect_true(
-      has_thousand_separator_with_decimals(
+      has_thousand_sep_with_two_dec(
         gsub("Rp. ", "", format_bukti_value(rand_num))
       )
     )
   })
 
   it("formats datetime to '%b %d, %Y %H:%M:%S' format", {
-    format <-  '%b %d, %Y %H:%M:%S'
+    format <-  "%b %d, %Y %H:%M:%S"
     expect_true(
       !is.na(
         strptime(format_bukti_value(rand_dttm), format)
@@ -46,3 +48,50 @@ describe("format_bukti_value()", {
   })
 })
 
+describe("get_bukt()", {
+  value1 <- random_string()
+  value2 <- random_string()
+
+  class1 <- "bukti__text--dark"
+  class2 <- "bukti__text--light"
+
+  tagq <- tagQuery(
+    get_bukti_text(
+      value1 = value1,
+      value2 = value2
+    )
+  )
+
+  it("assigns bukti__text--dark to value1 and bukti__text--light to value2", {
+    expect_true(tagq$hasClass(class1)[1])
+    expect_true(tagq$hasClass(class2)[2])
+  })
+})
+
+describe("bukti_button_item", {
+  url <- "https://www.google.com/"
+  out <- bukti_button_item(
+    url = url,
+    value1 = random_string(),
+    value2 = random_string()
+  )
+  tagq <- tagQuery(out)
+
+  it("returns an HTML button", {
+    expect_true(
+      grepl("^<button .*</button>$", out)
+    )
+  })
+
+  it("assigns bukti__button class to output", {
+    expect_true(tagq$hasClass("bukti__button"))
+  })
+
+  it("adds window.open(url, '_blank'); method in onclick event to url input", {
+    expect_true(tagq$hasAttrs("onclick"))
+    expect_equal(
+      glue("window.open('{url}','_blank');"),
+      tagGetAttribute(out, "onclick")
+    )
+  })
+})
