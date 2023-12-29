fb1fdcb7e5b194a6b869cc996adb799961a7e8b5
Tanesya <tanesya.t@gmail.com>
2023-12-29 17:32:36 +0700

    chore: update unit tests

--- tests/testthat/test-tracker.R
+++ tests/testthat/test-tracker.R
@@ -10,27 +10,35 @@ box::use(
   app/logic/tracker[...]
 )
 
-
 describe("rename_sheet_cols()", {
+  cols_rules <- list(
+    name1 = random_string(),
+    name2 = random_string(),
+    name3 = random_string()
+  )
 
-  it("rename columns based on cols_rules input", {
-    cols_rules <- list(
-      name1 = random_string(),
-      name2 = random_string(),
-      name3 = random_string()
+  dat <- random_data_frame(
+    list(
+      columns = list(
+        name1 = list(type = "string"),
+        name2 = list(type = "integer"),
+        name3 = list(type = "string")
+      )
+    ),
+    size = 6
+  )
+
+  it("rename columns based on cols_rules' value by default", {
+    expect_equal(
+      unname(unlist(cols_rules)),
+      colnames(rename_sheet_cols(dat, cols_rules))
     )
+  })
 
-    dat <- random_data_frame(
-      list(
-        columns = list(
-          name1 = list(type = "string"),
-          name2 = list(type = "integer"),
-          name3 = list(type = "string")
-        )
-      ),
-      size = 6
+  it("rename columns based on cols_rules' key when revert=TRUE", {
+    expect_equal(
+      names(cols_rules),
+      colnames(rename_sheet_cols(dat, cols_rules, revert = TRUE))
     )
-
-    expect_equal(names(cols_rules), colnames(rename_sheet_cols(dat, cols_rules)))
   })
 })
