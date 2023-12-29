5fdcb84c2f6eea979b3400670e07815c3786a9c8
Tanesya <tanesya.t@gmail.com>
2023-12-29 17:22:00 +0700

    fix: ci error - JS linter

--- app/js/index.js
+++ app/js/index.js
@@ -1,13 +1,13 @@
-$(document).ready(function(){
-    const navLinks = document.querySelectorAll('.nav-link');
-    navLinks.forEach((item) => {
-        if (item.href === window.location.href || item.href.replace(/#!\/$/,'') == window.location.href) {
-          item.classList.add('active')
-        }
-    });
+$(document).ready(() => {
+  const navLinks = document.querySelectorAll('.nav-link');
+  navLinks.forEach((item) => {
+    if (item.href === window.location.href || item.href.replace(/#!\/$/, '') === window.location.href) {
+      item.classList.add('active');
+    }
+  });
 
-    $('.nav-link').click(function(){
-      $('.nav-link').removeClass("active");
-      $(this).addClass("active");
-    });
+  $('.nav-link').click(function () {
+    $('.nav-link').removeClass('active');
+    $(this).addClass('active');
+  });
 });
